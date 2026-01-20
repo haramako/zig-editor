75e9f2cd0220ef5a2dedc27c3f24e306f9dee6ec
HARADA Makoto <haramako@gmail.com>
2026-01-20 09:16:47 +0900

    Change.

--- src/keybinding.zig
+++ src/keybinding.zig
@@ -5,14 +5,12 @@ const types = @import("types.zig");
 const key_sequence_processor = @import("key_sequence_processor.zig");
 
 const KeyBinding = struct {
-    sequence: []types.Key,
-    pub fn init(gpa: std.mem.Allocator, sequence: []types.Key) !KeyBinding {
-        const new_sequence = try gpa.alloc(types.Key, sequence.len);
-        @memcpy(new_sequence, sequence);
-        return .{ .sequence = new_sequence };
-    }
-    pub fn deinit(self: *KeyBinding, gpa: std.mem.Allocator) void {
-        gpa.free(self.sequence);
+    len: usize = undefined,
+    sequence: [4]types.Key = undefined,
+    pub fn init(sequence: []types.Key) !KeyBinding {
+        var new_sequence: [4]types.Key = undefined;
+        @memcpy(&new_sequence, sequence[0..new_sequence.len]);
+        return .{ .len = sequence.len, .sequence = new_sequence };
     }
 };
 
@@ -20,9 +18,9 @@ const KeyBinding = struct {
 /// Example:
 ///   "up" -> KeyBinding{ .sequence = [.{ .Command = .Up }] }
 ///   "C-x M-s" -> KeyBinding{ .sequence = [.{ .Control = 'X' }, .{ .Alt = 's' }] }
-pub fn parseKeyBinding(gpa: std.mem.Allocator, _str: []const u8) !KeyBinding {
+pub fn parseKeyBinding(_str: []const u8) !KeyBinding {
     const str = std.mem.trimStart(u8, _str, " ");
-    var keys: [8]types.Key = undefined;
+    var keys: [4]types.Key = undefined;
     var count: usize = 0;
     var iterator = std.mem.splitAny(u8, str, " ");
     while (iterator.next()) |part| {
@@ -35,14 +33,12 @@ pub fn parseKeyBinding(gpa: std.mem.Allocator, _str: []const u8) !KeyBinding {
         count += 1;
     }
 
-    return KeyBinding.init(gpa, keys[0..count]);
+    return KeyBinding.init(keys[0..count]);
 }
 
 const t = std.testing;
 test "parseKeyBinding" {
-    const gpa = std.testing.allocator;
-    var kb1 = try parseKeyBinding(gpa, "Up Down C-x M-s");
-    defer kb1.deinit(gpa);
+    var kb1 = try parseKeyBinding("Up Down C-x M-s");
 
     try t.expectEqual(4, kb1.sequence.len);
     try t.expectEqual(types.Key{ .Command = .Up }, kb1.sequence[0]);
